4a9bb28634aac9d5ea91cf064789430c8c18147f
Malek Hammou <46074100+malekhammou@users.noreply.github.com>
2022-01-30 00:49:39 +0100

    Add files via upload
    added button handlers

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,10 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.beans.EventHandler;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,12 +20,17 @@ import javax.swing.JTextField;
  *
  * @author Malek Hammou
  */
-public class main {
+public class main  {
+
+    private static String Operation;
     public static void main(String[] args) {
+        String operator="";
+        /*NEW FRAME*/
         JFrame mainFrame=new JFrame();
         mainFrame.setTitle("CALCULATOR");
         mainFrame.setSize(300,200);
         mainFrame.setLayout(new BorderLayout());
+        /*USED FOR DISPLAY*/
         JTextField ioField=new JTextField();
         mainFrame.add(ioField,BorderLayout.NORTH);
         JPanel digitsPanel=new JPanel();
@@ -29,28 +38,63 @@ public class main {
         for (int i = 1; i < 10; i++) {
             JButton button=new JButton(""+i);
             button.setForeground(Color.blue);
+            button.addActionListener(actionEvent ->  {
+                
+                ioField.setText(ioField.getText()+((JButton)actionEvent.getSource()).getText());
+});
             digitsPanel.add(button);
         }
-        /* ADD ZERO BUTTON*/
+      
+        
+        /*ADD ZERO BUTTON*/
         JButton zeroButton=new JButton(""+0);
+  
         zeroButton.setForeground(Color.blue);
         digitsPanel.add(zeroButton);
-        /* ADD SIGN BUTTON */
+        /*ADD SIGN BUTTON*/
         JButton signButton=new JButton("+/-");
+        signButton.addActionListener(actionEvent ->  {
+            int nb = Integer.parseInt(ioField.getText().toString());
+            nb=-nb;   
+                ioField.setText(String.valueOf(nb));
+});
         signButton.setForeground(Color.blue);
         digitsPanel.add(signButton);
         /* ADD CLEAR ALL BUTTON*/
         JButton clearAllButton=new JButton("CLEAR");
         clearAllButton.setForeground(Color.PINK);
+        clearAllButton.addActionListener(actionEvent ->  {
+                ioField.setText("");
+});
         digitsPanel.add(clearAllButton);
         
         mainFrame.add(digitsPanel,BorderLayout.CENTER);
         JPanel operationsPanel=new JPanel();
         operationsPanel.setLayout(new GridLayout(4,1));
+        /*ADDITION*/
         JButton additionButton=new JButton("+");
+        additionButton.addActionListener(actionEvent ->  {
+            Operation="-";
+            ioField.setText(ioField.getText()+"+");
+});
+        /*SUBSTRACTION*/
         JButton substractionButton=new JButton("-");
+        substractionButton.addActionListener(actionEvent ->  {
+            Operation="-";
+            ioField.setText(ioField.getText()+"-");
+});
+        /* DIVISION*/
         JButton divisionButton=new JButton("/");
+        divisionButton.addActionListener(actionEvent ->  {
+            Operation="/";
+            ioField.setText(ioField.getText()+"/");
+});
+        /*MULTIPLICATION*/
         JButton multiplicationButton=new JButton("*");
+        multiplicationButton.addActionListener(actionEvent ->  {
+            Operation="*";
+            ioField.setText(ioField.getText()+"*");
+});
         operationsPanel.add(additionButton);
         operationsPanel.add(substractionButton);
         operationsPanel.add(multiplicationButton);
@@ -59,21 +103,13 @@ public class main {
         divisionButton.setForeground(Color.red);
         substractionButton.setForeground(Color.red);
         multiplicationButton.setForeground(Color.red);
-
-                        
+           
         mainFrame.add(operationsPanel,BorderLayout.EAST);
         mainFrame.add(new JButton("ENTER"),BorderLayout.SOUTH);
-
-
-
-
-        
-
-        
-      
         mainFrame.setVisible(true);
 
-        
-        
+   
     }
-}
+
+    }
+
